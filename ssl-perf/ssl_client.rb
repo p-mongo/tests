3e286cb9afb859fdaff8f1f21df5c8481d76d439
Oleg Pudeyev <oleg@bsdpower.com>
2018-06-27 17:38:29 -0400

    Nonblocking client

--- ssl-perf/ssl_client.rb
+++ ssl-perf/ssl_client.rb
@@ -10,7 +10,15 @@ ssl_socket = OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
 ssl_socket.sync_close = true
 ssl_socket.connect
 
-while data = ssl_socket.read(1_000_000)
-  puts data.length
+while true
+  begin
+    data = ssl_socket.read_nonblock(1_000_000)
+    puts data.length
+  rescue OpenSSL::SSL::SSLErrorWaitReadable
+    sleep 0.1
+    retry
+  rescue EOFError
+    break
+  end
 end
 ssl_socket.close
