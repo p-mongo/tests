9baa86508e474f5f6410c3b36b23038a3793f7db
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-09 14:29:03 -0400

    Add unicorn

--- reconnect-after-fork-driver/app.rb
+++ reconnect-after-fork-driver/app.rb
@@ -2,13 +2,18 @@ require 'mongo'
 require 'sinatra'
 
 $client = Mongo::Client.new(ENV['MONGODB_URI'] || 'mongodb://localhost:27017')
-
-get '/' do
-  doc = $client['test'].find(a: {'$ne' => nil}).first
-  doc.to_json
+10.times do
+  $client['test'].insert_one(a: 'x'*15000000)
 end
 
-post '/' do
-  $client['test'].insert_one(params)
-  'ok'
+class App < Sinatra::Base
+  get '/' do
+    doc = $client['test'].find(a: {'$ne' => nil}).first
+    doc.to_json
+  end
+
+  post '/' do
+    $client['test'].insert_one(params)
+    'ok'
+  end
 end
---
+++ reconnect-after-fork-driver/config.ru
@@ -0,0 +1,5 @@
+$: << Dir.pwd
+
+require 'app'
+
+run App
