68ea50deacfc8df49532b57f2a3ba3428f07be28
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-30 15:39:01 -0400

    More fields

--- mongoid-app/app/models/post.rb
+++ mongoid-app/app/models/post.rb
@@ -2,6 +2,10 @@ class Post
   include Mongoid::Document
 
   field :user_id, type: String
+  field :title, type: String
+  field :bar, type: String
+
+  #validates_uniqueness_of :bar
 
   belongs_to :user, optional: true
 end
