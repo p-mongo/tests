65f7c381ddf2121a32c34716f0d484bfd05be3d6
Oleg Pudeyev <oleg@bsdpower.com>
2018-10-02 22:42:01 -0400

    Mongoid reproducer

---
+++ no-timeout-cursor-cleanup/doit-mongoid.rb
@@ -0,0 +1,26 @@
+require 'byebug'
+require 'mongoid'
+
+Mongoid.configure do |config|
+  config.clients.default = {
+    hosts: ['localhost:27100'],
+    database: 'my_db',
+  }
+
+  config.log_level = :warn
+end
+
+class Post
+  include Mongoid::Document
+
+  field :title, type: String
+end
+
+#10000.times { Post.create!(title: 'blah') }
+
+1.times { Post.where(title: 'blah').no_timeout.first }
+
+GC.start
+
+sleep 1
+puts 'done'
