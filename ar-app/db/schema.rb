b98973eef1f4bd81aa14de9592af91ccf3a1f7bc
Oleg Pudeyev <oleg@bsdpower.com>
2018-10-26 11:08:14 -0400

    fk test

--- ar-app/db/schema.rb
+++ ar-app/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_174017) do
+ActiveRecord::Schema.define(version: 2018_10_26_144822) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "c"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string "c_fk"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
