2f109e4394a44ac1e6e5b43967fb0da800707e01
Oleg Pudeyev <oleg@bsdpower.com>
2019-02-15 16:48:42 -0500

    Drop AR

--- mongoid-app/config/application.rb
+++ mongoid-app/config/application.rb
@@ -48,7 +48,8 @@ Mongo::Monitoring::Global.subscribe(Mongo::Monitoring::COMMAND, subscriber)
 
 require_relative 'boot'
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
