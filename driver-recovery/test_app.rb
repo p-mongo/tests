d9810dc0c14507ade58491e2ec8cd58b9a101921
Oleg Pudeyev <oleg@bsdpower.com>
2019-09-03 20:11:13 -0400

    wip on app testing

--- driver-recovery/test_app.rb
+++ driver-recovery/test_app.rb
@@ -1,30 +1,7 @@
-require 'faraday'
-begin
-  require 'byebug'
-rescue LoadError
-end
+require_relative './tester'
 
-class Tester
-  def initialize(uri)
-    @uri = uri
-    @threads = []
-    @read_ops = 0
-    @exception_count = 0
-    @lock = Mutex.new
-  end
-
-  attr_reader :uri
-  attr_reader :exception_count
-
-  def logger
-    @logger ||= Logger.new(File.open('client.log', 'w'))
-  end
-
-  def reader_thread_count
-    20
-  end
-
-  def run
+class AppTester < Tester
+  def run_delegated
     reader_thread_count.times do |i|
       client = Faraday.new(:url => @uri) do |faraday|
         faraday.request  :url_encoded             # form-encode POST params
@@ -55,72 +32,13 @@ class Tester
         end
       end
     end
-
-    @threads << run_thread('monitor') do
-      run_monitor
-    end
-
-    Signal.trap('INT') do
-      puts 'Stopping'
-      @stop = true
-    end
-
-    until @stop
-      sleep 1
-    end
-
-    @threads.map(&:join)
   end
 
-  def run_thread(thread_label)
-    Thread.new do
-      begin
-        yield
-      rescue => e
-        puts "Unhandled exception in thread #{thread_label}: #{e.class}: #{e}"
-        #puts e.backtrace.join("\n")
-        @lock.synchronize do
-          @exception_count += 1
-        end
-      end
-    end
-  end
-
-  def run_thread_loop(thread_label)
-    Thread.new do
-      until @stop
-        begin
-          yield
-        rescue => e
-          puts "Unhandled exception in thread #{thread_label}: #{e.class}: #{e}"
-          #puts e.backtrace.join("\n")
-          @lock.synchronize do
-            @exception_count += 1
-          end
-        end
-      end
-    end
-  end
-
-  def run_monitor
-    prev_read_ops = @lock.synchronize { @read_ops }
-    until @stop
-      sleep 1
-      read_ops = @lock.synchronize do
-        @read_ops
-      end
-      alive_threads_count = @threads.select do |thread|
-        thread.alive?
-      end.length
-      exception_count = @lock.synchronize do
-        self.exception_count
-      end
-      puts "#{Time.now}: #{read_ops - prev_read_ops} read ops, " +
-        "#{alive_threads_count} alive threads, #{exception_count} exceptions"
-      prev_read_ops = read_ops
-    end
+  def perform_operation
+    collection.find(a: 1).to_a
   end
 end
 
-uri = ARGV.shift || 'http://localhost:9292'
-Tester.new(uri).run
+config_path = ARGV.shift or raise 'need config file'
+config = YAML.load(File.read(config_path)).deep_symbolize_keys
+AppTester.new(config).run
--- driver-recovery/test_client.rb
+++ driver-recovery/test_client.rb
@@ -1,87 +1,7 @@
-require 'childprocess'
-begin
-  require 'byebug'
-rescue LoadError
-end
-require 'mongo'
-require 'active_support/core_ext/hash'
+require_relative './tester'
 
-ChildProcess.posix_spawn = true
-
-class OurTimeoutError < StandardError; end
-
-class Tester
-  def initialize(options)
-    @options = options
-    @threads = []
-    @read_ops = 0
-    @exception_count = 0
-    @lock = Mutex.new
-  end
-
-  attr_reader :options
-  attr_reader :exception_count
-  attr_reader :start_time
-
-  def logger
-    log_path = options[:driver_log] || 'client.log'
-    @logger ||= Logger.new(File.open(log_path, 'w'))
-  end
-
-  def client
-    @client ||= Mongo::Client.new(options[:uri], logger: logger,
-      **options[:client_options] || {})
-  end
-
-  def collection
-    @collection ||= client['test-collection'].with(options[:collection_options] || {})
-  end
-
-  def reader_thread_count
-    options[:application_threads] || 20
-  end
-
-  def prepare
-    if options[:preexec]
-      options[:preexec].each do |cmd|
-        execute(cmd)
-      end
-    end
-  end
-
-  def execute(cmd)
-    if sh_cmd = cmd[:sh]
-      puts "Run #{sh_cmd}"
-      proc = ChildProcess.new('sh', '-c', sh_cmd)
-      proc.io.inherit!
-      proc.start
-      proc.wait
-      unless proc.exit_code == 0
-        raise "Failed to execute #{cmd} (exited with code #{proc.exit_code})"
-      end
-    elsif cmd[:exit]
-      puts "Exiting"
-      @stop = true
-    else
-      raise "Don't know what to do with #{cmd}"
-    end
-  end
-
-  def run
-    prepare
-
-    logger
-    client
-    collection
-
-    @start_time = Time.now
-
-    if options[:exec]
-      Thread.new do
-        do_exec
-      end
-    end
-
+class ClientTester < Tester
+  def run_delegated
     reader_thread_count.times do |i|
       @threads << run_thread_loop("reader-#{i}") do
         begin
@@ -92,125 +12,13 @@ class Tester
         sleep 0.01
       end
     end
-
-    @threads << run_thread('monitor') do
-      run_monitor
-    end
-
-    Signal.trap('INT') do
-      puts 'Stopping'
-      @stop = true
-    end
-
-    until @stop
-      sleep 1
-    end
-
-    @threads.map(&:join)
   end
 
-  def target_ops
-    options[:target_ops] || 100
-  end
-
-  def do_run_loop_iteration
-    target_time = Time.now + 1
-    ops = 0
-    while Time.now < target_time
-      do_one_find
-      ops += 1
-      if ops >= target_ops
-        break
-      end
-    end
-    delta = target_time - Time.now
-    if delta > 0
-      sleep delta
-    end
-  end
-
-  def do_one_find
-    if options[:application_timeout]
-      Timeout.timeout(options[:application_timeout], OurTimeoutError) do
-        do_find_itself
-      end
-    else
-      do_find_itself
-    end
-  end
-
-  def do_find_itself
+  def perform_operation
     collection.find(a: 1).to_a
-    @lock.synchronize do
-      @read_ops += 1
-    end
-  end
-
-  def run_thread(thread_label)
-    Thread.new do
-      begin
-        yield
-      rescue => e
-        puts "Unhandled exception in thread #{thread_label}: #{e.class}: #{e}"
-        #puts e.backtrace.join("\n")
-        @lock.synchronize do
-          @exception_count += 1
-        end
-      end
-    end
-  end
-
-  def run_thread_loop(thread_label)
-    Thread.new do
-      until @stop
-        begin
-          yield
-        rescue => e
-          puts "Unhandled exception in thread #{thread_label}: #{e.class}: #{e}"
-          #puts e.backtrace.join("\n")
-          @lock.synchronize do
-            @exception_count += 1
-          end
-        end
-      end
-    end
-  end
-
-  def run_monitor
-    prev_read_ops = @lock.synchronize { @read_ops }
-    until @stop
-      sleep 1
-      read_ops = @lock.synchronize do
-        @read_ops
-      end
-      alive_threads_count = @threads.select do |thread|
-        thread.alive?
-      end.length
-      exception_count = @lock.synchronize do
-        self.exception_count
-      end
-      now = Time.now
-      puts "#{now} [+#{'%.0f' % (now - start_time)}]: #{read_ops - prev_read_ops} read ops, " +
-        "#{alive_threads_count} alive threads, #{exception_count} exceptions"
-      puts client.cluster.summary
-      prev_read_ops = read_ops
-    end
-  end
-
-  def do_exec
-    options[:exec].each do |time_delta, cmd|
-      target_time = @start_time + time_delta
-      wait_delta = target_time - Time.now
-      if wait_delta > 0
-        sleep(wait_delta)
-      end
-
-      puts "At #{time_delta}, run #{cmd}"
-      execute(cmd)
-    end
   end
 end
 
 config_path = ARGV.shift or raise 'need config file'
 config = YAML.load(File.read(config_path)).deep_symbolize_keys
-Tester.new(config).run
+ClientTester.new(config).run
