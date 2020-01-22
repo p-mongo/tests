1fa17c3d62f6a534c71e183919bd38923196905c
Oleg Pudeyev <oleg@bsdpower.com>
2020-01-22 14:36:37 -0500

    test

--- mongoid-hash-symbol/test.rb
+++ mongoid-hash-symbol/test.rb
@@ -9,7 +9,19 @@ class Model
   field :stuff, type: Hash
 end
 
+Model.delete_all
+
+=begin
 m = Model.create!(stuff: {a: :b})
 p m
 m = Model.find(m.id)
 p m
+=end
+
+puts '-------------'
+
+d = Model.collection.insert_one(stuff: {a: :b})
+p d.documents.first
+d = Model.find(d['id'])
+p d
+d
