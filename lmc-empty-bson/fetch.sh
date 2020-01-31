66009d1fa1a64618d60ab48e186656148b48a17d
Oleg Pudeyev <oleg@bsdpower.com>
2020-01-31 17:17:49 -0500

    empty bson test

---
+++ lmc-empty-bson/fetch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+
+curl https://s3.amazonaws.com/mciuploads/libmongocrypt/all/master/latest/libmongocrypt-all.tar.gz |tar zxf -
---
+++ lmc-empty-bson/test.c
@@ -0,0 +1,12 @@
+#include "mongocrypt.h"
+
+int main() {
+  mongocrypt_binary_t *bin_p;
+  mongocrypt_t *handle_p;
+  mongocrypt_ctx_t *ctx_p;
+  
+  bin_p = mongocrypt_binary_new_from_data("", 0);
+  handle_p = mongocrypt_new();
+  ctx_p = mongocrypt_ctx_new(handle_p);
+  mongocrypt_ctx_decrypt_init(ctx_p, bin_p);
+}
