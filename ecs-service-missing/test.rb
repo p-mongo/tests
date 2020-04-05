32b5332280575542eb0f22b4010c74b479229391
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-04 23:09:57 -0400

    missing ecs service test

---
+++ ecs-service-missing/test.rb
@@ -0,0 +1,57 @@
+require 'aws-sdk-ecs'
+
+cluster_name = ENV.fetch('IAM_AUTH_ECS_CLUSTER')
+
+client = Aws::ECS::Client.new(
+  region: 'us-east-1',
+  credentials: Aws::Credentials.new(
+    ENV.fetch('IAM_AUTH_ECS_ACCOUNT'),
+    ENV.fetch('IAM_AUTH_ECS_SECRET_ACCESS_KEY'),
+  ),
+)
+
+stop = false
+prefix = Time.now.to_i
+1.upto(100) do |i|
+  service_name = "test_service_#{prefix}_#{i}"
+
+  service = client.create_service(
+    desired_count: 1,
+    service_name: service_name,
+    task_definition: ENV.fetch('IAM_AUTH_ECS_TASK_DEFINITION'),
+    cluster: cluster_name,
+    launch_type: 'FARGATE',
+    network_configuration: {
+      awsvpc_configuration: {
+        subnets: [ENV.fetch('IAM_AUTH_ECS_SUBNET_A')],
+        security_groups: [ENV.fetch('IAM_AUTH_ECS_SECURITY_GROUP')],
+      },
+    },
+  ).service
+
+  p service
+
+  service = client.describe_services(
+    cluster: cluster_name,
+    services: [service_name],
+  ).services.first
+
+  if service
+    puts "Round #{i}: got a service for #{service.service_name}"
+  else
+    puts "Round #{i}: did not get a service for #{service.service_name}"
+    stop = true
+  end
+
+  client.update_service(
+    cluster: cluster_name,
+    service: service.service_name,
+    desired_count: 0,
+  )
+  client.delete_service(
+    cluster: cluster_name,
+    service: service.service_name,
+  )
+
+  break if stop
+end
