eb3184b0c0cf1dc123d9846a03288c95f83ca238
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-09 00:14:40 -0400

    mm init

--- shard-maint/entrypoint.sh
+++ shard-maint/entrypoint.sh
@@ -2,4 +2,16 @@
 
 set -e
 
+mongo-manager --dir /db init --mongos 3 --replica-set shrs --nodes 3
+
+echo
+echo -------------------------------
+echo
+echo Database initialized
+echo
+echo -------------------------------
+echo
+
+ps awwxu
+
 exec "$@"
