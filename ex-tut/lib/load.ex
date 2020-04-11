7cc6de01e4f14f4ed1f93f1fdfd2191ff23a6822
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-11 12:27:24 -0400

    skeleton

---
+++ ex-tut/lib/load.ex
@@ -0,0 +1,18 @@
+defmodule Load do
+  @moduledoc """
+  Documentation for `Load`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Load.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
