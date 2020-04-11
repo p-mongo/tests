8b6fb2243d777f921e43340d13dfd567c84abd7c
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-11 15:15:07 -0400

    run app

--- ex-tut/lib/load.ex
+++ ex-tut/lib/load.ex
@@ -15,4 +15,12 @@ defmodule Load do
   def hello do
     :world
   end
+  
+  use Application
+  
+  # https://stackoverflow.com/questions/30687781/how-to-run-elixir-application
+  def start(_type, _args) do
+    IO.puts("starting!")
+    Supervisor.start_link [], strategy: :one_for_one
+  end
 end
--- ex-tut/mix.exs
+++ ex-tut/mix.exs
@@ -14,6 +14,7 @@ defmodule Load.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Load, []},
       extra_applications: [:logger]
     ]
   end
