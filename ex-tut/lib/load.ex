bf92950282a22704ebe83e82c982e0621093e43e
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-11 20:40:53 -0400

    loop infinitely

--- ex-tut/lib/load.ex
+++ ex-tut/lib/load.ex
@@ -24,29 +24,36 @@ defmodule Load do
     
     # Starts an unpooled connection
     {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:14420/load",
-      pool_size: 10)
+      pool_size: 100)
 
-    worker = fn ->
-      # Gets an enumerable cursor for the results
-      cursor = Mongo.find(conn, "coll",
-        %{"$and" => [%{a: "hello"}]}
-      )
-
-      cursor
-      |> Enum.to_list()
-      |> Enum.count
-      |> IO.inspect
-    end
     
     Enum.map(1..100, fn i ->
-      spawn_link worker
+      spawn_link fn -> read(conn) end
     end)
     
     receive do
     after
-      10_000 -> "done"
+      100_000 -> "done"
     end
 
     Supervisor.start_link [], strategy: :one_for_one
+  end
+  
+  defp read(conn) do
+    worker(conn)
+    
+    read(conn)
+  end
+
+  defp worker(conn) do
+    # Gets an enumerable cursor for the results
+    cursor = Mongo.find(conn, "coll",
+      %{"$and" => [%{a: "hello"}]}
+    )
+
+    cursor
+    |> Enum.to_list()
+    |> Enum.count
+    |> IO.inspect
   end
 end
