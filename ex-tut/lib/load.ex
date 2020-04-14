459c03403d87908263e0902e4ce5a4f08b1ecde1
Oleg Pudeyev <oleg@bsdpower.com>
2020-04-13 22:20:54 -0400

    write load

--- ex-tut/lib/load.ex
+++ ex-tut/lib/load.ex
@@ -23,12 +23,9 @@ defmodule Load do
     IO.puts("starting!")
     
     {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:14420,localhost:14421,localhost:14422/load",
-      pool_size: 1000)
+      pool_size: 10)
 
-    
-    Enum.map(1..1000, fn i ->
-      spawn_link fn -> read(conn) end
-    end)
+    write_many(conn)
     
     receive do
     after
@@ -38,13 +35,25 @@ defmodule Load do
     Supervisor.start_link [], strategy: :one_for_one
   end
   
+  defp read_many(conn) do
+    Enum.map(1..1000, fn i ->
+      spawn_link fn -> read(conn) end
+    end)
+  end
+  
+  defp write_many(conn) do
+    Enum.map(1..20, fn i ->
+      spawn_link fn -> write(conn) end
+    end)
+  end
+  
   defp read(conn) do
-    worker(conn)
+    read_worker(conn)
     
     read(conn)
   end
 
-  defp worker(conn) do
+  defp read_worker(conn) do
     # Gets an enumerable cursor for the results
     cursor = Mongo.find(conn, "coll",
       %{"$and" => [%{a: "hello"}]}
@@ -54,5 +63,18 @@ defmodule Load do
     |> Enum.to_list()
     |> Enum.count
     |> IO.inspect
+  end
+  
+  defp write(conn) do
+    write_worker(conn)
+    
+    write(conn)
+  end
+
+  defp write_worker(conn) do
+    Mongo.insert_one!(conn, "coll",
+      %{a: "hello"},
+      w: 1,
+    )
   end
 end
