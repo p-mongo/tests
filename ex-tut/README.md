ed42c3027db99b578c42e961285df267a8efd13a
Oleg Pudeyev <oleg@bsdpower.com>
2020-05-28 00:21:42 -0400

    tweak

--- ex-tut/README.md
+++ ex-tut/README.md
@@ -1,21 +1,5 @@
-# Load
+# Load Tester
 
-**TODO: Add description**
+Run:
 
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `load` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:load, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/load](https://hexdocs.pm/load).
-
+    mix
--- ex-tut/lib/load.ex
+++ ex-tut/lib/load.ex
@@ -1,26 +1,10 @@
 defmodule Load do
-  @moduledoc """
-  Documentation for `Load`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Load.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
   
   use Application
   
   # https://stackoverflow.com/questions/30687781/how-to-run-elixir-application
   def start(_type, _args) do
-    IO.puts("starting!")
+    IO.puts("starting load tester")
     
     {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:14420,localhost:14421,localhost:14422/load",
       pool_size: 10)
