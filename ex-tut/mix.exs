a5a9e61f3f2ad349ec6268da3cccd34e3885b2cc
Oleg Pudeyev <oleg@bsdpower.com>
2020-05-29 14:52:02 -0400

    add statix

--- ex-tut/mix.exs
+++ ex-tut/mix.exs
@@ -24,6 +24,7 @@ defmodule Load.MixProject do
   defp deps do
     [
       {:mongodb_driver, "~> 0.6"},
+      {:statix, ">= 0.0.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
