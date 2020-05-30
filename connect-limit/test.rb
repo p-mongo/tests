0788c206bdbcb007806b3015e323f04014a8ca79
Oleg Pudeyev <oleg@bsdpower.com>
2020-05-29 22:06:47 -0400

    connect limit test

---
+++ connect-limit/test.rb
@@ -0,0 +1,15 @@
+require 'mongo'
+
+Mongo::Logger.logger.level = Logger::INFO
+
+client = Mongo::Client.new(['localhost:34420'],
+  min_pool_size: 20_000, max_pool_size: 100_000)
+
+servers = client.cluster.servers.sort_by { |s| s.address.seed }
+
+loop do
+  puts(servers.map do |s|
+    "#{s.address.seed}: #{'%-5d' % s.pool.size}"
+  end.join(' '))
+  sleep 1
+end
