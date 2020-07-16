6715df1e29c5651926b50bb80cbac1f483b7721f
Oleg Pudeyev <oleg@bsdpower.com>
2020-07-16 02:14:35 -0400

    supervisor attempt

---
+++ ex-tut/lib/supervisor.ex
@@ -0,0 +1,17 @@
+defmodule Load.Supervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  @impl true
+  def init(:ok) do
+    children = [
+      Load.Reporter |
+      (1..10 |> Enum.map(fn -> {Reader} end))
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
