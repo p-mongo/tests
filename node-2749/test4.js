6d3870e68fc5294d85232df9fbd857d3c294a408
Oleg Pudeyev <oleg@bsdpower.com>
2020-08-05 12:33:28 -0400

    2749

---
+++ node-2749/test4.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:14070,localhost:14071/?retrywrites=false&directconnection=false"
+
+const client = new MongoClient(uri,{useUnifiedTopology:true});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
+    // create a document to be inserted
+    const doc = { name: "Red", town: "kanto" };
+    const result = await collection.insertOne(doc);
+
+    console.log(
+      `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
