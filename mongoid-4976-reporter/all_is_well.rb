f7159ff258354890393063b96e93c656de557029
Oleg Pudeyev <oleg@bsdpower.com>
2020-10-18 16:02:48 -0400

    reporter

---
+++ mongoid-4976-reporter/all_is_well.rb
@@ -0,0 +1,31 @@
+=begin
+All is well if no own attributes present.
+Exception raised as expected.
+=end
+
+require 'mongoid'
+
+
+Mongoid.load!(File.join(File.dirname(__FILE__), 'mongoid_config.yml'), :development)
+
+
+class User
+	include Mongoid::Document
+
+	embeds_one	:auth_data,	class_name: 'UserAuthData',	autobuild: true
+	accepts_nested_attributes_for	:auth_data
+	validates						:auth_data,	presence: true
+end
+
+
+class UserAuthData
+	include Mongoid::Document
+
+	embedded_in :user
+	validates	:email,  presence: true, allow_blank: false
+
+	field  :email,  type: String
+end
+
+
+User.create! auth_data: {}
---
+++ mongoid-4976-reporter/doesnt_throw_as_expected.rb
@@ -0,0 +1,30 @@
+=begin
+Doesn't throw as expected if own attribute present.
+=end
+
+require 'mongoid'
+
+
+Mongoid.load!(File.join(File.dirname(__FILE__), 'mongoid_config.yml'), :development)
+
+
+class User
+	include Mongoid::Document
+
+	embeds_one	:auth_data,	class_name: 'UserAuthData',	autobuild: true
+	accepts_nested_attributes_for	:auth_data
+	validates						:auth_data,	presence: true
+end
+
+
+class UserAuthData
+	include Mongoid::Document
+
+	embedded_in :user
+	validates	:email,  presence: true, allow_blank: false
+
+	field  :email,  type: String
+end
+
+
+User.create!
