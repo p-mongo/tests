45880fed0c1eedcc19a095a939990beadd0fec56
Oleg Pudeyev <oleg@bsdpower.com>
2021-02-17 22:22:11 -0500

    fix the test

--- atlas-stress-rate-limit/test-dns.rb
+++ atlas-stress-rate-limit/test-dns.rb
@@ -1,15 +1,11 @@
-#require 'async/dns'
-require 'mongo'
-require 'awesome_print'
-require 'mongo_cloud'
-autoload :Byebug, 'byebug'
+require 'resolv'
 
 10_000.times do |i|
   Resolv::DNS.open(nameserver: '127.0.0.1', nameserver_port: [['127.0.0.1', 53]]) do |dns|
-      ress = dns.getresources 'cloud-dev.mongodb.com', Resolv::DNS::Resource::IN::A
-      if ress.empty?
-        raise 'Uh oh here'
-      end
+    ress = dns.getresources 'cloud-dev.mongodb.com', Resolv::DNS::Resource::IN::A
+    if ress.empty?
+      raise 'Uh oh here'
+    end
   end
   print('.')
   if i > 0 && i % 100 == 0
