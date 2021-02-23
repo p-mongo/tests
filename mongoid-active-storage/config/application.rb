1e6005997a1bdff3ecb04fec501bca5a30d9c505
Oleg Pudeyev <oleg@bsdpower.com>
2021-02-23 14:18:01 -0500

    get working

--- mongoid-active-storage/config/application.rb
+++ mongoid-active-storage/config/application.rb
@@ -5,7 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 # require "active_record/railtie"
-# require "active_storage/engine"
+#require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "action_mailbox/engine"
@@ -33,5 +33,7 @@ module RailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << 'serene'
   end
 end
