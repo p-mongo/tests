4b885d58ca458db9f48f15918187547f5f9fff17
Oleg Pudeyev <oleg@bsdpower.com>
2021-02-23 19:08:56 -0500

    activestorage with ar

--- ar-app/config/application.rb
+++ ar-app/config/application.rb
@@ -15,5 +15,9 @@ module NestedTransactionsAr
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_storage.service = :local
+
+    config.hosts << 'serene'
   end
 end
