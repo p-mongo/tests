9cf83844f31902b6c5ad5f478365dd0e140d032d
Oleg Pudeyev <oleg@bsdpower.com>
2021-04-22 22:54:41 -0400

    updated repro

--- mongoid-5075/test.rb
+++ mongoid-5075/test.rb
@@ -1,25 +1,40 @@
 #!/usr/bin/env ruby
 
+
+
 require 'bundler/inline'
 
+
+
 gemfile do
+
   source 'https://rubygems.org'
+
   gem 'mongoid', '7.0.12'
+
 end
+
+
 
 require 'mongoid'
 
+
+
 Mongoid.configure { |c| c.clients.default = { hosts: ['localhost:14920'], database: 'test' } }
+
+
 
 puts Mongoid::VERSION
 
 puts Mongo::VERSION
 
+
+
 class Article
 
   include Mongoid::Document
 
-  has_many :comments, autosave: true
+  has_many :comments
 
 end
 
@@ -29,40 +44,47 @@ class Comment
 
   include Mongoid::Document
 
-  belongs_to :article
+  belongs_to :article, required: false
 
 end
 
 
-    comment = Comment.create
-    p comment
 
-    article = Article.create(
-
-      comment_ids: [comment.id],
-
-      id: '123',
-
-    )
-    p article
+comment = Comment.create!
+p comment
 
 
 
-    pp article.comments.count #=> 1
+Article.destroy_all
 
-    pp article.comments.criteria.selector['article_id'] # => Gives a random id
+article = Article.create!(
 
-    pp article.comments.context.view.count # => 1
+  comment_ids: [comment.id],
+
+  id: '1234',
+
+)
+
+p article
+p comment
+p article.comments.first
 
 
 
-    article.reload
+pp article.comments.count #=> 1
+
+pp article.comments.criteria.selector['article_id'] # => Gives a random id
+
+pp article.comments.context.view.count # => 1
 
 
 
-    pp article.comments.count # => 0
+article.reload
 
-    pp article.comments.criteria.selector['article_id'] #=> '123'
 
-    pp article.comments.context.view.count # => 0
 
+pp article.comments.count # => 0
+
+pp article.comments.criteria.selector['article_id'] #=> '123'
+
+pp article.comments.context.view.count # => 0
