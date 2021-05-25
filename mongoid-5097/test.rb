e249fc706ff1e25a7f3d088c8af1a22d0d2cbe81
Oleg Pudeyev <oleg@bsdpower.com>
2021-05-25 17:34:16 -0400

    5097

--- mongoid-5097/test.rb
+++ mongoid-5097/test.rb
@@ -7,6 +7,7 @@ gemfile(true) do
   source "https://rubygems.org"
 
   gem "mongoid", "~> 7.3.0"
+  gem 'byebug'
 
 end
 
@@ -23,8 +24,41 @@ puts Mongo::VERSION
 
 
 
-class MyModel;include Mongoid::Document;field :attr1, type: Time;end
+class MyModel
+  include Mongoid::Document
+
+  field :a, type: Time
+end
 
 
+#pp MyModel.any_of({:a.exists => true}, a: nil)
 
-MyModel.any_of({ :attr1.exists => true }, { attr1: nil })
+require 'byebug'
+byebug
+exit
+
+
+pp MyModel.any_of(a: 42)
+
+=begin
+#<Mongoid::Criteria
+  selector: {"a"=>1970-01-01 00:00:42 UTC}
+  options:  {}
+  class:    MyModel
+  embedded: false>
+=end
+
+pp MyModel.any_of(:a.gt => 42)
+
+=begin
+#<Mongoid::Criteria
+  selector: {"a"=>{"$gt"=>1970-01-01 00:00:42 UTC}}
+  options:  {}
+  class:    MyModel
+  embedded: false>
+=end
+
+p 'exists'
+pp MyModel.any_of(:a.exists => true)
+pp MyModel.any_of(:a.exists => true).or(a: nil)
+pp MyModel.any_of({:a.exists => true}, {a: nil})
