39af7df7f1bacec682ef565e4e21504b8466f3b6
Oleg Pudeyev <code@olegp.name>
2022-04-05 19:02:08 -0400

    test

--- mongoid-app/app/models/band.rb
+++ mongoid-app/app/models/band.rb
@@ -4,33 +4,5 @@ class Band
   field :name, type: String
   field :description, type: String
   field :likes, type: Integer
-
-  index description: 'text'
-
-  around_save :foo, :bar
-
-  def foo
-    #byebug
-    yield
-  end
-
-  def bar
-    #byebug
-    yield
-  end
-
-  def _after_save
-    #byebug
-    puts 'after save'
-  end
-
-  embeds_one :cat, cascade_callbacks: true
-
-  around_save :as
-  def as
-    p 'band around save enter'
-    yield
-    p 'band around save exit'
-  end
-
+  field :year, type: Array
 end
