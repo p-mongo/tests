cc2e2503318cc74af7d818d6f3dda4966cf2b395
Oleg Pudeyev <code@olegp.name>
2022-06-08 23:55:47 -0400

    3252

---
+++ mongoid-3252/test.rb
@@ -0,0 +1,44 @@
+require 'mongoid'
+
+Mongoid.configure do |config|
+  config.clients.default = {
+    hosts: ['localhost:14400'],
+    database: 'junk_db',
+    options: {
+      truncate_logs: false
+    }
+  }
+  config.raise_not_found_error = false
+end
+
+logger = Logger.new(STDOUT)
+logger.level = :debug
+Mongoid.logger = logger
+Mongo::Logger.logger = logger
+
+# the big container object
+class Book
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  embeds_many :notes
+end
+
+# the template "note"
+class Note
+  include Mongoid::Document
+  embedded_in :book
+  field :message, :type => String
+end
+
+book = Book.create!
+p book.updated_at
+
+sleep 1
+
+book.notes << Note.new
+book.save!
+p book.updated_at
+
+book.notes.first.message = 'hello'
+book.save!
+p book.updated_at
