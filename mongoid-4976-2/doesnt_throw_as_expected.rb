83db15557e79e2bd515b3b44ae1105a5404cf5bd
Oleg Pudeyev <code@olegp.name>
2022-06-17 23:37:11 -0400

    reduce

--- mongoid-4976-2/doesnt_throw_as_expected.rb
+++ mongoid-4976-2/doesnt_throw_as_expected.rb
@@ -10,25 +10,24 @@ Mongoid.load!(File.join(File.dirname(__FILE__), 'mongoid_config.yml'), :developm
 
 
 class User
-	include Mongoid::Document
+  include Mongoid::Document
 
-	embeds_one	:auth_data,	class_name: 'UserAuthData',	autobuild: true
-	accepts_nested_attributes_for	:auth_data
-	validates						:auth_data,	presence: true
+  embeds_one :auth_data, class_name: 'UserAuthData', autobuild: true
+  validates :auth_data,	presence: true
 end
 
 
 class UserAuthData
-	include Mongoid::Document
+  include Mongoid::Document
 
-	embedded_in :user
-	validates	:email,  presence: true, allow_blank: false
+  embedded_in :user
+  validates :email,  presence: true, allow_blank: false
 
-	field  :email,  type: String
+  field :email, type: String
 end
 
 
 u = User.create!
 p u
-p u.auth_data
+#p u.auth_data
 p u.valid?
